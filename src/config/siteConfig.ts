1258a21d69dfa674b87f91f1bd0c3928058844e4
hakersX <3154621254@qq.com>
2026-08-16 23:38:25 +0800

    加了追番页

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -104,19 +104,19 @@ export const siteConfig: SiteConfig = {
 	// 页面开关配置 - 控制特定页面的访问权限，设为false会返回404并自动隐藏对应的导航栏菜单项
 	pages: {
 		// 友链页面开关
-		friends: false,
+		friends: true,
 		// 打赏页面开关
 		sponsor: false,
 		// 留言板页面开关，需要配置评论系统
 		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
-		bangumi: false,
+		bangumi: true,
 		// VNDB页面开关。
 		vndb: true,
 		// 相册页面开关
 		gallery: true,
 		// 追番页面开关
-		anime: false,
+		anime: true,
 		// 动态页面开关
 		dynamic: true,
 		// 书签导航页面开关
@@ -265,7 +265,7 @@ export const siteConfig: SiteConfig = {
 		// Bilibili 配置
 		bilibili: {
 			// 你的 Bilibili 用户 UID
-			uid: "38932988",
+			uid: "356139592",
 		},
 		// TMDB 配置（可选，需要翻墙）
 		// tmdb: {
