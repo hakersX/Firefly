3df7c090ad7a0befb116db4c8dea5596526e8bbf
hakersX <3154621254@qq.com>
2026-08-17 09:28:14 +0800

    新的首页图，适配了手机端打开

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -110,9 +110,9 @@ export const siteConfig: SiteConfig = {
 		// 留言板页面开关，需要配置评论系统
 		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
-		bangumi: true,
+		bangumi: false,
 		// VNDB页面开关。
-		vndb: true,
+		vndb: false,
 		// 相册页面开关
 		gallery: true,
 		// 追番页面开关
